e6c44e872e9ee57290c7ca4f80934e7b30b475f2
Johannes Steck <johannes-steck@gmx.de>
2015-11-19 09:10:33 +0100

    test retriever

--- webcontentextractor/src/main/java/com/jms/tools/webtools/webcontentextractor/App.java
+++ webcontentextractor/src/main/java/com/jms/tools/webtools/webcontentextractor/App.java
@@ -1,5 +1,9 @@
 package com.jms.tools.webtools.webcontentextractor;
 
+import java.io.IOException;
+
+import org.jsoup.nodes.Document;
+
 /**
  * Hello world!
  *
@@ -9,5 +13,15 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        WebDocumentRetriever wdr = new WebDocumentRetriever();
+        try {
+			Document d = wdr.getWebDocument("http://www.proxy-listen.de/Proxy/Proxyliste.html");
+		
+        System.out.println("Doc:\r\nw"+d.html());
+        } catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
     }
 }
